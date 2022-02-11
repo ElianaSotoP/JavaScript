c4064a829629b699a2e978c334fb9c096d687c4b
ElianaSoto <eliana.soto.pacheco0106@gmail.com>
2022-02-10 20:56:32 -0500

    agregando algunos comentarios y alerts

--- jsJuego.js
+++ jsJuego.js
@@ -1,4 +1,22 @@
-//en esta seccion estoy tomando 
+//
+let nombre = prompt('Hola, Ingresa tu nombre: ');
+let edad = parseInt(prompt('Ingresa tu edad'))
+if (edad<18){
+  alert(`${nombre} estás en la edad perfecta para jugar!`)
+  console.log(`${nombre} estás en la edad perfecta para jugar!`)
+} else if (edad<35){
+  alert(`${nombre} debes estar estresado con las labores, asi que este juego es ideal para ti! `)
+  console.log(`${nombre} debes estar estresado con las labores, asi que este juego es ideal para ti! `)
+} else{
+  alert(`${nombre} no te preocupes para este juego no  existe barreras de edad! `)
+  console.log(`${nombre} no te preocupes para este juego no  existe barreras de edad! `)
+}
+
+alert('HAZ CLICK EN EMPEZAr A JUGAR' )
+
+
+
+//en esta seccion estoy tomando los ID de los colores y tambien del bonton empezar 
 const celeste = document.getElementById('celeste')
 const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
@@ -36,6 +54,9 @@ class Juego {
 //Esta funcion se ejecuta despues de haber dado click en empezar jueg 
 function empezarJuego() {
   //en esta parte ya tendra una secuencia de 10 numeros de 0 al 3 en un array 
+  alert('INGRESA LA PALABRA juego EN CONSOLA' )
   window.juego = new Juego()
+
 }
+
 
