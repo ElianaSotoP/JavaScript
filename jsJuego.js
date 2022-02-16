f24fa8cd9dd7114565825b22f088a9dbacf77609
ElianaSoto <eliana.soto.pacheco0106@gmail.com>
2022-02-15 20:15:55 -0500

    logre iluminar el color de mi juego

--- jsJuego.js
+++ jsJuego.js
@@ -1,5 +1,5 @@
-//
-let nombre = prompt('Hola, Ingresa tu nombre: ');
+/* 
+let nombre = prompt('Hola, Ingresa tu nombre: /* ');
 let edad = parseInt(prompt('Ingresa tu edad'))
 if (edad<18){
   alert(`${nombre} estás en la edad perfecta para jugar!`)
@@ -11,52 +11,77 @@ if (edad<18){
   alert(`${nombre} no te preocupes para este juego no  existe barreras de edad! `)
   console.log(`${nombre} no te preocupes para este juego no  existe barreras de edad! `)
 }
+ 
+alert("HAZ CLICK EN EMPEZAr A JUGAR");  */
 
-alert('HAZ CLICK EN EMPEZAr A JUGAR' )
-
-
-
-//en esta seccion estoy tomando los ID de los colores y tambien del bonton empezar 
-const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
-const btnEmpezar = document.getElementById('btnEmpezar')
+//en esta seccion estoy tomando los ID de los colores y tambien del boton empezar
+const celeste = document.getElementById("celeste");
+const violeta = document.getElementById("violeta");
+const naranja = document.getElementById("naranja");
+const verde = document.getElementById("verde");
+const btnEmpezar = document.getElementById("btnEmpezar");
 
 class Juego {
   constructor() {
-    this.inicializar()
-    this.generarSecuencia()
+    this.inicializar();
+    this.generarSecuencia();
+    this.siguienteNivel();
   }
 
   inicializar() {
-    btnEmpezar.classList.add('hide')
-    //Para el nivel del juego 
+    btnEmpezar.classList.add("hide");
+    //Para el nivel del juego
     this.nivel = 1;
-    this.colores ={
+    //DEFINIR DE MANERA GLOBAL ---
+    this.colores = {
       //celeste: celeste,
       celeste,
       violeta,
       naranja,
-      verde,
-
-    }
-
+      verde
+    };
   }
-  generarSecuencia(){
-    //definiendo la secuencia internamente e un atributo del juego 
+  generarSecuencia() {
+    //definiendo la secuencia internamente e un atributo del juego
     //gerando array de numeros random
-    // en esta parte estoy generando 10 numeros randon del 0 al 3 
+    // en esta parte estoy generando 10 numeros randon del 0 al 3
     this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+  }
+  siguienteNivel() {
+    this.iluminarSecuencia();
+  }
+  //Funcion que transforma de numero a color
+  transformarNumeroAColor(numero) {
+    switch (numero) {
+      case 0:
+        return "celeste";
+      case 1:
+        return "violeta";
+      case 2:
+        return "naranja";
+      case 3:
+        return "verde";
+    }
+  }
+  iluminarSecuencia() {
+    for (let i = 0; i < this.nivel; i++) {
+      const color = this.transformarNumeroAColor(this.secuencia[i]);
+      setTimeout(()=>this.iluminarColor(color),100000 * i) 
+    }
+  }
+  iluminarColor(color){
+    //En esta seccion estoy haciendo llamando mi clase light del color, de esa manera hago una aparente iluminacion  
+    this.colores[color].classList.add('light')
+    setTimeout(()=>this.apagarColor(color),350)
 
   }
+  apagarColor(color){
+    this.colores[color].classList.remove('light')
+  }
 }
-//Esta funcion se ejecuta despues de haber dado click en empezar jueg 
+//Esta funcion se ejecuta despues de haber dado click en empezar juegar
 function empezarJuego() {
-  //en esta parte ya tendra una secuencia de 10 numeros de 0 al 3 en un array 
-  alert('INGRESA LA PALABRA juego EN CONSOLA' )
-  window.juego = new Juego()
-
+  //en esta parte ya tendra una secuencia de 10 numeros de 0 al 3 en un array
+ // alert("INGRESA LA PALABRA juego EN CONSOLA");
+  window.juego = new Juego();
 }
-
-
